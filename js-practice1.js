5db001dc0cebadde2b835d9ee7244490d11e0271
shahin2525 <shahin451999@gmail.com>
2024-03-04 01:05:38 -0800

    first commit

---
+++ js-practice1.js
@@ -0,0 +1,58 @@
+// numbers.pop();
+// numbers.shift();
+// numbers.unshift(80, 100);
+// numbers.shift(1);
+// const popElement = numbers.pop();
+// console.log(popElement);
+// const isGraduated = true;
+// const salary = 60000;
+// const cars = 0;
+// if ((isGraduated === true && salary > 50000) || cars >= 1) {
+//   console.log("aso biya kori");
+// } else {
+//   console.log("tor kopaley biya nai");
+// }
+
+const num1 = 13;
+const num2 = 79;
+const num3 = 45;
+// if (num1 > num2 && num1 > num3) {
+//   console.log(num1, "is largest");
+// } else if (num2 > num1 && num2 > num3) {
+//   console.log(num2, "is largest");
+// } else {
+//   console.log(num3, "is largest");
+// }
+
+// let roastGiven = 0;
+// while (roastGiven < 7) {
+//   console.log("roast given");
+//   console.log(roastGiven);
+//   roastGiven++;
+// }
+
+// let number = 1;
+// while (number <= 10) {
+//   console.log(number);
+//   number = number + 2;
+// }
+/* console.log("simple for loop");
+for (let fruits = 0; fruits < 7; fruits++) {
+  console.log(fruits);
+} */
+
+// for (let number = 0; number <= 10; number += 2) {
+//   console.log(number);
+// }
+
+// for (let number = 1; number <= 10; number += 2) {
+//   console.log(number);
+// }
+
+for (let i = 0; i <= 10; i = i + 2) {
+  console.log(i);
+}
+
+for (let i = 1; i <= 10; i = i + 2) {
+  console.log(i);
+}
