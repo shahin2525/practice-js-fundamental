45a28c0b454ba70266562ad66f027d46e1bac51e
shahin2525 <shahin451999@gmail.com>
2024-03-07 21:44:20 -0800

    add event listener added

--- js/event.js
+++ js/event.js
@@ -6,3 +6,18 @@ makePurple.onclick = function makePurple() {
 function makeTomato() {
   document.body.style.backgroundColor = "tomato";
 }
+
+const handleStatus = () => {
+  const statusText = document.getElementById("handle-status");
+  statusText.innerText = "status text has been changed by on clicked";
+};
+
+//
+document.getElementById("update").addEventListener("click", function () {
+  const input = document.getElementById("input-field");
+  const inputText = input.value;
+
+  const text = document.getElementById("text");
+  text.innerText = inputText;
+  input.value = "";
+});
