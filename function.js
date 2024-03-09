4bd942ac5a17fc0a27a9dbc62f5af6ba5a09de90
shahin2525 <shahin451999@gmail.com>
2024-03-09 00:21:02 -0800

    rerurn function 2

--- function.js
+++ function.js
@@ -11,3 +11,26 @@
 // };
 
 // console.log(bringSingara(100));
+
+// const sum = (a, b, c, d) => {
+//   console.log(a, b, c, d);
+//   const sum = a + b + c + d;
+//   return sum;
+// };
+// const a = 2;
+// const b = 4;
+// const c = 6;
+// const d = 8;
+
+// const addSum = sum(a, b, c, d);
+// console.log(addSum);
+
+const bringSingara = (money) => {
+  const singaraPrice = 10;
+  const quantity = money / singaraPrice;
+  return quantity;
+};
+
+const money = 1000;
+const getSingara = bringSingara(money);
+console.log(getSingara);
