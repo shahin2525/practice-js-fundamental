6bc692bb1a5949c634bdf858e2a8e89a9a94030a
shahin2525 <shahin451999@gmail.com>
2024-03-10 09:41:56 -0700

    practice function more

--- function.js
+++ function.js
@@ -25,12 +25,32 @@
 // const addSum = sum(a, b, c, d);
 // console.log(addSum);
 
-const bringSingara = (money) => {
-  const singaraPrice = 10;
-  const quantity = money / singaraPrice;
-  return quantity;
-};
+// const bringSingara = (money) => {
+//   const singaraPrice = 10;
+//   const quantity = money / singaraPrice;
+//   return quantity;
+// };
 
-const money = 1000;
-const getSingara = bringSingara(money);
-console.log(getSingara);
+// const money = 1000;
+// const getSingara = bringSingara(money);
+// console.log(getSingara);
+
+// const getAverage = (as1, as2, as3) => {
+//   const total = as1 + as2 + as3;
+//   const average = total / 3;
+//   return average;
+// };
+
+// const as1 = 58;
+// const as2 = 57;
+// const as3 = 59;
+// const myAvarage = getAverage(as1, as2, as3);
+// console.log(myAvarage);
+
+const add = (num1, num2) => {
+  const sum = num1 + num2;
+  return sum;
+};
+const result1 = add(10, 20);
+const result2 = add(30, 40);
+const finalResult = add(result1, result2);
