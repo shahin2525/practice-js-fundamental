20bfa638e2a87e3f1bfd1b1f437a195089c1fe90
shahin2525 <shahin451999@gmail.com>
2024-03-10 22:54:57 -0700

    focus event added

--- js/more-event.js
+++ js/more-event.js
@@ -3,3 +3,7 @@ document
   .addEventListener("mouseover", function () {
     console.log("clicked");
   });
+//
+document.getElementById("input").addEventListener("focus", function () {
+  console.log("triggered input");
+});
