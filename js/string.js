66230bc106e33604a8aee8cfc3a299fedccc3cc4
shahin2525 <shahin451999@gmail.com>
2024-03-24 19:59:39 -0700

    practice string immutible

---
+++ js/string.js
@@ -0,0 +1,8 @@
+const userName = "blAckPink";
+const userInput = "blackPinK";
+console.log(userInput.length);
+if (userName.toLowerCase() === userInput.toLowerCase()) {
+  console.log("valid user");
+} else {
+  console.log("Invalid user");
+}
