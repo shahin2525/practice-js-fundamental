31226acc08bc02dc1d51a1349936d88ca9f02d03
shahin2525 <shahin451999@gmail.com>
2024-03-24 22:19:04 -0700

    check number

--- js/string.js
+++ js/string.js
@@ -1,5 +1,5 @@
 const userName = "14245";
-const userInput = "blackPinK";
+const userInput = 12;
 console.log(userInput.length);
 if (userName.toLowerCase() === userInput.toLowerCase()) {
   console.log("valid user");
